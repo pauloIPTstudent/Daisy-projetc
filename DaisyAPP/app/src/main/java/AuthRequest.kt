219b93f7ce8b127421bbfc5716d10d18dd89985d
Paulo Daniel Sentinella De Paula <aluno24880@ipt.pt>
2025-12-22 17:02:27 +0000

    SingUP
    Implementa a lógica do sing-up

--- DaisyAPP/app/src/main/java/AuthRequest.kt
+++ DaisyAPP/app/src/main/java/AuthRequest.kt
@@ -1,10 +1,10 @@
 // O que envias no POST
-data class LoginRequest(
+data class AuthRequest(
     val email: String,
     val password: String
 )
 
 // O que recebes (a tua resposta 200 OK)
-data class LoginResponse(
+data class AuthResponse(
     val token: String
 )
