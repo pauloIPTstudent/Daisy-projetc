e6fecc2ffafb25cfba323ce2d4b0ffb332c25655
Paulo Daniel Sentinella De Paula <aluno24880@ipt.pt>
2025-12-23 02:21:34 +0000

    navbottom-navigationo

--- DaisyAPP/app/src/main/java/com/example/daisyapp/MainActivity.kt
+++ DaisyAPP/app/src/main/java/com/example/daisyapp/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.daisyapp
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var icons: List<ImageView>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,7 +19,57 @@ class MainActivity : AppCompatActivity() {
         if (!isLogged()) {
             gotoAuth()
         }
+        // Inicializa a lista de ícones
+        icons = listOf(
+            findViewById(R.id.nav_home),
+            findViewById(R.id.nav_diagnose),
+            findViewById(R.id.nav_camera),
+            findViewById(R.id.nav_yard),
+            findViewById(R.id.nav_explore)
+        )
+
+        // Configura o clique para cada um
+        icons.forEach { icon ->
+            icon.setOnClickListener {
+                handleNavigation(it.id)
+            }
+        }
+
+        // Carrega o fragmento inicial (Home)
+        if (savedInstanceState == null) {
+            handleNavigation(R.id.nav_home)
+        }
     }
+
+    fun handleNavigation(selectedId: Int) {
+        val fragment = when (selectedId) {
+            R.id.nav_home -> HomeFragment()
+            /*R.id.nav_diagnose -> DiagnoseFragment()
+            R.id.nav_camera -> CameraFragment()
+            R.id.nav_yard -> YardFragment()
+            //R.id.nav_explore -> ExploreFragment()*/
+            else -> HomeFragment()
+        }
+
+        // Troca o Fragmento
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView3, fragment)
+            .commit()
+
+        // Atualiza as cores dos ícones
+        updateIconColors(selectedId)
+    }
+
+    private fun updateIconColors(selectedId: Int) {
+        icons.forEach { icon ->
+            if (icon.id == selectedId) {
+                icon.imageTintList = ColorStateList.valueOf(Color.parseColor("#8BC34A")) // Verde
+            } else {
+                icon.imageTintList = ColorStateList.valueOf(Color.parseColor("#BDBDBD")) // Cinza
+            }
+        }
+    }
+
     private fun gotoAuth() {
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
